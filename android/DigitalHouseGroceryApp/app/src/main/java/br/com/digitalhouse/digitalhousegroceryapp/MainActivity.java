290d63c8736867452a04de88fffa3e1d868fd74c
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2019-06-13 23:31:37 -0300

    main activity

--- android/DigitalHouseGroceryApp/app/src/main/java/br/com/digitalhouse/digitalhousegroceryapp/MainActivity.java
+++ android/DigitalHouseGroceryApp/app/src/main/java/br/com/digitalhouse/digitalhousegroceryapp/MainActivity.java
@@ -1,8 +1,13 @@
 package br.com.digitalhouse.digitalhousegroceryapp;
 
 import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,9 +27,52 @@ public class MainActivity extends AppCompatActivity {
         String email = bundle.getString("EMAIL");
 
         // Buscar elemento na tela por id
-        TextView emailText = findViewById(R.id.email_text);
+//        TextView emailText = findViewById(R.id.email_text);
 
         // Alterar o texto
-        emailText.setText("Usuário: "+email);
+//        emailText.setText("Usuário: "+email);
+
+        Button comprasButton = findViewById(R.id.compras_button);
+        comprasButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exibirComprasFragment();
+            }
+        });
+
+        Button listaSalvaButton = findViewById(R.id.lista_button);
+        listaSalvaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exibirListasFragment();
+            }
+        });
+
+        Button promocaoButton = findViewById(R.id.promocoes_button);
+        promocaoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exibirPromocoesFragment();
+            }
+        });
+    }
+
+    private void exibirComprasFragment() {
+        setupFragment(new ComprasFragment());
+    }
+
+    private void exibirListasFragment() {
+        setupFragment(new ListaSalvaFragment());
+    }
+
+    private void exibirPromocoesFragment() {
+        setupFragment(new PromocaoFragment());
+    }
+
+    private void setupFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.container_id, fragment);
+        transaction.commit();
     }
 }
---
+++ android/FragmentApp/app/src/main/java/br/com/digitalhouse/fragmentapp/Comunicador.java
@@ -0,0 +1,4 @@
+package br.com.digitalhouse.fragmentapp;
+
+public interface Comunicador {
+}
