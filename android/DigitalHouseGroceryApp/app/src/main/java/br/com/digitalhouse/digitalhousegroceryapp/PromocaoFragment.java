2b34a4ba7d3d7ff5297ad5fad82e03b4b5e2ba2d
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2019-08-09 09:54:18 -0300

    recycler view

--- android/DigitalHouseGroceryApp/app/src/main/java/br/com/digitalhouse/digitalhousegroceryapp/PromocaoFragment.java
+++ android/DigitalHouseGroceryApp/app/src/main/java/br/com/digitalhouse/digitalhousegroceryapp/PromocaoFragment.java
@@ -3,9 +3,14 @@ package br.com.digitalhouse.digitalhousegroceryapp;
 
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import br.com.digitalhouse.digitalhousegroceryapp.adapter.PromocaoAdapter;
 
 
 /**
@@ -23,7 +28,13 @@ public class PromocaoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_promocao, container, false);
+        View view = inflater.inflate(R.layout.fragment_promocao, container, false);
+
+        RecyclerView recyclerView = view.findViewById(R.id.promocao_recycler_view_id);
+        recyclerView.setAdapter(new PromocaoAdapter());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+         return view;
     }
 
 }
