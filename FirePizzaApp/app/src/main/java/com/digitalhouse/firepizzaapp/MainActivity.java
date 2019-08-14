607be38b369afd39a2ca922ad5226aa69dfd6714
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2019-08-14 11:07:47 -0300

    notificatiokn

--- FirePizzaApp/app/src/main/java/com/digitalhouse/firepizzaapp/MainActivity.java
+++ FirePizzaApp/app/src/main/java/com/digitalhouse/firepizzaapp/MainActivity.java
@@ -1,22 +1,36 @@
 package com.digitalhouse.firepizzaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.io.ByteArrayOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView saudacaoTextView;
     private ImageView perfilImageView;
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private FirebaseUser user;
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -28,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
         saudacaoTextView = findViewById(R.id.saudacao_text_view_id);
         perfilImageView = findViewById(R.id.perfil_image_view_id);
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             String name = user.getDisplayName();
             saudacaoTextView.setText("Bem vindo "+name);
@@ -41,6 +55,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        baixarFoto();
+
+    }
+
+    private void baixarFoto() {
+
+        StorageReference reference = storage.getReference("perfil/"+user.getUid());
+
+        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(perfilImageView);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                Toast.makeText(MainActivity.this, "Erro ao baixar foto", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void tirarFoto() {
@@ -51,11 +85,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
+            Bundle extras = intent.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             perfilImageView.setImageBitmap(imageBitmap);
+
+            StorageReference reference = storage.getReference("perfil/"+user.getUid());
+
+            // Get the data from an ImageView as bytes
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] data = baos.toByteArray();
+
+            UploadTask uploadTask = reference.putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Toast.makeText(MainActivity.this, "Upload falhou", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(MainActivity.this, "Upload concluído", Toast.LENGTH_SHORT).show();
+                }
+            });
+
         }
     }
 
---
+++ FirePizzaApp/app/src/main/java/com/digitalhouse/firepizzaapp/service/PizzaFirebaseMessagingService.java
@@ -0,0 +1,7 @@
+package com.digitalhouse.firepizzaapp.service;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class PizzaFirebaseMessagingService extends FirebaseMessagingService {
+
+}
