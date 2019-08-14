51135679e37f6979eae6d5607abbac07130658ee
Fabio Tadashi Miyasato <miyasato.fabio@gmail.com>
2019-08-14 09:25:08 -0300

    foto

--- FirePizzaApp/app/src/main/java/com/digitalhouse/firepizzaapp/MainActivity.java
+++ FirePizzaApp/app/src/main/java/com/digitalhouse/firepizzaapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +16,9 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private TextView saudacaoTextView;
+    private ImageView perfilImageView;
+
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +26,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         saudacaoTextView = findViewById(R.id.saudacao_text_view_id);
+        perfilImageView = findViewById(R.id.perfil_image_view_id);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         if (user != null) {
             String name = user.getDisplayName();
-
             saudacaoTextView.setText("Bem vindo "+name);
-
         }
 
+        perfilImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tirarFoto();
+            }
+        });
+
+    }
+
+    private void tirarFoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            perfilImageView.setImageBitmap(imageBitmap);
+        }
     }
 
 }
